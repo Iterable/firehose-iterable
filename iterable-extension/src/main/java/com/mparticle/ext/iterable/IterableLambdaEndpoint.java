d3d304a7919db2e989e94d63eb11cd936d25be67
julianmclain <julianrmclain@gmail.com>
2020-07-26 23:14:29 -0700

    Parse SQS trigger

--- iterable-extension/src/main/java/com/mparticle/ext/iterable/IterableLambdaEndpoint.java
+++ iterable-extension/src/main/java/com/mparticle/ext/iterable/IterableLambdaEndpoint.java
@@ -1,8 +1,8 @@
 package com.mparticle.ext.iterable;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mparticle.sdk.model.Message;
 import com.mparticle.sdk.model.MessageSerializer;
 import org.apache.commons.io.IOUtils;
@@ -11,20 +11,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-
 public class IterableLambdaEndpoint implements RequestStreamHandler {
 
-    static MessageSerializer serializer = new MessageSerializer();
-    static IterableExtension processor = new IterableExtension();
+  static MessageSerializer serializer = new MessageSerializer();
+  static IterableExtension processor = new IterableExtension();
+  static ObjectMapper mapper = new ObjectMapper();
 
-    @Override
-    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-        // TODO - remove
-        String messageBody = IOUtils.toString(input, "UTF-8");
-        System.out.println("Received message from queue: " + messageBody);
+  @Override
+  public void handleRequest(InputStream input, OutputStream output, Context context)
+      throws IOException {
+    // TODO - remove
+    String inputString = IOUtils.toString(input, "UTF-8");
+    System.out.println("Received trigger from queue: " + inputString);
 
-        Message request = serializer.deserialize(messageBody, Message.class);
-        Message response = processor.processMessage(request);
-        serializer.serialize(output, response);
-    }
+    Message request = parseQueueTrigger(inputString);
+    Message response = processor.processMessage(request);
+    serializer.serialize(output, response);
+  }
+
+  public static Message parseQueueTrigger(String triggerString) throws IOException {
+    QueueTrigger trigger = mapper.readValue(triggerString, QueueTrigger.class);
+    String messageBody = trigger.records.get(0).body;
+    Message request = serializer.deserialize(messageBody, Message.class);
+    return request;
+  }
 }
